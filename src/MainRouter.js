6ce3fe567ab141e5f089d3e202e4b05559424aa0
Life Toolbox <42096132+touminh@users.noreply.github.com>
2023-10-22 14:12:41 +0700

    init

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,13 +1,15 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Index from './renderer/screen/Index';
-const MainRouter = props=>{
-  return(
+
+function MainRouter() {
+  return (
     <Router>
       <Routes>
         <Route path="/" element={<Index />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default MainRouter;
--- src/renderer/screen/Index.js
+++ src/renderer/screen/Index.js
@@ -1,16 +1,8 @@
-import {Container,Row,Col} from 'react-bootstrap';
-const Index = props=>{
-  return(
-
-
-
-        <Container>
-          <Row>
-            <Col>1 of 1</Col>
-          </Row>
-        </Container>
-    
-
-  )
+function Index() {
+  return (
+    <div>
+      <h1>Index</h1>
+    </div>
+  );
 }
 export default Index;
